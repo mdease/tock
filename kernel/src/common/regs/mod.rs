02ad573ac4d70e96325339d8bdd3c71e0e2f070b
Brad Campbell <bradjc5@gmail.com>
2018-03-24 10:00:57 -0400

    kernel regs interface

--- kernel/src/common/regs/mod.rs
+++ kernel/src/common/regs/mod.rs
@@ -122,6 +122,11 @@ impl<T: IntLike, R: RegisterLongName> ReadWrite<T, R> {
     }
 
     #[inline]
+    pub fn replace(&self, field: LocalRegisterCopy<T, R>) {
+        self.set(field.value);
+    }
+
+    #[inline]
     pub fn write(&self, field: FieldValue<T, R>) {
         self.set(field.value);
     }
@@ -148,6 +153,13 @@ impl<T: IntLike, R: RegisterLongName> ReadWrite<T, R> {
     }
 }
 
+/// This behaves very similarly to a read-only register, but instead of doing a
+/// volatile read to MMIO to get the value for each function call, a copy of the
+/// register contents are stored locally in memory. This allows a peripheral
+/// to do a single read on a register, and then check which bits are set without
+/// having to do a full MMIO read each time. It also allows the value of the
+/// register to be "cached" in case the peripheral driver needs to clear the
+/// register in hardware yet still be able to check the bits.
 pub struct LocalRegisterCopy<T: IntLike, R: RegisterLongName = ()> {
     value: T,
     associated_register: PhantomData<R>,
@@ -185,6 +197,17 @@ impl<T: IntLike, R: RegisterLongName> LocalRegisterCopy<T, R> {
     #[inline]
     pub fn matches_all(&self, field: FieldValue<T, R>) -> bool {
         self.value & field.mask == field.value
+    }
+}
+
+impl<T: IntLike, R: RegisterLongName> BitAnd for LocalRegisterCopy<T, R> {
+    type Output = Self;
+
+    fn bitand(self, LocalRegisterCopy(rhs): Self) -> Self {
+        LocalRegisterCopy {
+            value: self.value & rhs.value,
+            associated_register: self.associated_register
+        }
     }
 }
 
@@ -235,6 +258,11 @@ impl<T: IntLike, R: RegisterLongName> WriteOnly<T, R> {
             value: value,
             associated_register: PhantomData,
         }
+    }
+
+    #[inline]
+    pub fn replace(&self, field: LocalRegisterCopy<T, R>) {
+        self.set(field.value);
     }
 
     #[inline]
